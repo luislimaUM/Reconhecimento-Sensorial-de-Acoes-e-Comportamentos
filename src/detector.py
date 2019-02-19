6f7456868a9fd077eed13e25a7c24a4f7fa8b5e0
stabeyourface <luis.soccer.5@hotmail.com>
2019-02-19 00:42:52 +0000

    update detector

--- src/detector.py
+++ src/detector.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
-def LiveCamEdgeDetection_canny(image_color):
+def LiveCamEdgeDetection(image_color):
 
     # threshold1 – first threshold for the hysteresis procedure.
     # threshold2 – second threshold for the hysteresis procedure.
@@ -14,6 +14,10 @@ def LiveCamEdgeDetection_canny(image_color):
     image_gray = cv2.cvtColor(image_color, cv2.COLOR_BGR2GRAY)
     image_blurred = cv2.GaussianBlur(image_gray, (7, 7), 0)
     canny = cv2.Canny(image_blurred, threshold_1, threshold_2)
+    #cornerHarris(img,block size, ksize, k)
+    #corners = cv2.cornerHarris(canny, 5, 3, 0.1)
+    #corners_dilate = cv2.dilate(corners, np.ones((8,8),np.uint8), iterations = 1)
+    #image_color[corners > 0.01 * corners.max()] = [0,0,255]
     
     return canny
 
@@ -21,7 +25,7 @@ cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read() # Cap.read() returns a ret bool to indicate success.
-    cv2.imshow('Live Edge Detection', LiveCamEdgeDetection_canny(frame))
+    cv2.imshow('Webcam', LiveCamEdgeDetection(frame))
     cv2.imshow('Webcam Video', frame)
     if cv2.waitKey(1) == 13: #13 Enter Key
         break
